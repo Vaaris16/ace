bae554ce2773fa93ab66d9964c0dfc1d5504b73d
Vaaris Chitkara <vaarischitkara16@gmail.com>
2026-04-10 15:17:55 +0530

    made a basic thing

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Phantom")]
+#[command(about = "Ace cli")]
+struct Cli {
+    #[command(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { package: String },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.commands {
+        Commands::Add { package } => match package.as_str() {},
+    }
+}
